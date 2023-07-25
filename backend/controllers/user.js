6e1115e33e3a33ccc233a48b4c294bd0e2f90252
sudharshini23 <sudarsh25@gmail.com>
2023-07-25 13:01:22 +0530

    method 2 - async

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,62 @@
 const User = require('../models/details');
 
+// const getUserDetails = async (req,res,next) => {
+//     try {
+//         const users = await User.findAll();
+//         res.status(200).json({users: users})
+//     }
+//     catch(err) {
+//         console.log('Cant get user details', JSON.stringify(err));
+//         res.status(500).json({error: err})
+//     }
+// }
+
+// const postUserDetails = async (req,res,next) => {
+//     try {
+//         console.log(req.body);
+//         if(!req.body.phoneNumber) {
+//             throw new Error('Phone number is mandatory!')
+//         }
+//         if(!req.body.email) {
+//             throw new Error('Email ID is mandatory')
+//         }
+//         const name = req.body.userName; 
+//         const number = req.body.phoneNumber;
+//         const mail = req.body.email;
+
+//         const newUser = await User.create({
+//             userName: name,
+//             phoneNumber: number,
+//             email: mail
+//         });
+
+//         console.log(newUser);
+
+//         res.status(201).json({newUserDetail: newUser});
+//     }
+//     catch(err){
+//         res.status(500).json({error: err})
+//     }
+// }
+
+// const deleteUserDetails = async (req,res,next) => {
+//     const uId = req.params.id;
+//     if(uId === 'undefined') {
+//         console.log('ID is missing');
+//         return res.status(400).json({error: 'ID is missing'})
+//     }
+//     await User.destroy({where: {id: uId}});
+//     res.status(200);
+// }
+
+// module.exports = {
+//     getUserDetails,
+//     postUserDetails,
+//     deleteUserDetails
+// }
+
+// --------------- METHOD 2: USING EXPORTS ----------------------------------------
+
 exports.getUserDetails = (req,res,next) => {
     User.findAll()
     .then(users => {
